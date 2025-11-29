5eb1414c63c050523744984312114f249b6e777a
yusuftugradeveci23 <yusuftugradeveci23@gmail.com>
2025-11-29 21:26:38 +0300

    Update Exercise4_5.java

--- Exercise4_5.java
+++ Exercise4_5.java
@@ -1,27 +1,53 @@
+//Ad Soyad:Yusuf Tuğra Deveci
+//Öğrenci No:250541095
+//Tarih:29.11.2025
+//Açıklama:Bu program, Java’da metod çağrılarının çalışma sırasını ve stack üzerinde nasıl tutulduklarını göstermek amacıyla yazılmıştır.
+//Programda main(), zoop(), clink() ve ping() metotları bulunmaktadır.
+//Kod çalıştırıldığında metotlar birbirlerini çağırarak bir çağrı zinciri oluşturur.
+//Aşağıdaki bölümlerde bu çağrıların ürettiği çıktı ve o sırada oluşan stack yapısı açıklanmıştır.
+
+
 public class Exercise4_5 {
 
-    public static void zoop(String fred, int bob) {
-        System.out.println(fred);
-        if (bob == 5) {
-            ping("not ");
-        } else {
-            System.out.println("!");
+    public static void zoop(String fred, int bob) {//5 //15
+        System.out.println(fred);//6
+        if (bob == 5) {//7
+            ping("not ");//8
+        } else {//16
+            System.out.println("!");//17
         }
     }
 
-    public static void main(String[] args) {
-        int bizz = 5;
-        int buzz = 2;
-        zoop("just for", bizz);
-        clink(2 * buzz);
+    public static void main(String[] args) {//1
+        int bizz = 5;//2
+        int buzz = 2;//3
+        zoop("just for", bizz);//4
+        clink(2 * buzz);//11
     }
 
-    public static void clink(int fork) {
-        System.out.print("It's ");
-        zoop("breakfast ", fork);
+    public static void clink(int fork) {//12
+        System.out.print("It's ");//13
+        zoop("breakfast ", fork);//14
     }
 
-    public static void ping(String strangStrung) {
-        System.out.println("any " + strangStrung + "more ");
+    public static void ping(String strangStrung) {//9
+        System.out.println("any " + strangStrung + "more ");//10
     }
-}
+}  
+
+
+
+//zoop metodu ikinci kez ¸cağrıldığında programın durumunu gösteren
+//bir stack diyagramı ¸cizin.main():
+
+    //Stack Diyagramı:
+    // → zoop(" just for ", 5)
+    //  → ping(" not ")
+//  → clink(4)
+    //  → zoop(" breakfast ", 4
+
+
+//Çıktısı:just for 
+// any  not  more 
+//It ’s  breakfast 
+//!
