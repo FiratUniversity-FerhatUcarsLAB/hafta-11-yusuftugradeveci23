80be29be520f6e6021ce1072afdd91cf21900297
yusuftugradeveci23 <yusuftugradeveci23@gmail.com>
2025-11-29 18:12:51 +0300

    Update Exercise4_4.java

--- Exercise4_4.java
+++ Exercise4_4.java
@@ -3,19 +3,20 @@ public class Exercise4_4 {
     public static int getNumber() {
         return 42;
     }
+        // 1) Donus degerini kullanma
+    //Java herhangi bir hata vermez,kod çalışır sadece döndürülen değer görmezden gelinir.
 
     public static void sayHello() {
         System.out.println("hello");
     }
 
     public static void main(String[] args) {
-
-        // 1) Donus degerini kullanmayin -> ne oluyor?
+yin -> ne oluyor?
         getNumber();
 
         // 2) void metodu ifade icinde kullanin -> ne oluyor?
         // System.out.println(sayHello() + 7);
 
-        // Cevaplarinizi yorum olarak ekleyin.
+        // Cevap:Compile-time error verir kod çalışmaz. çünkü void yani döndürülemeyen değeri println içinde kullanılamaz.
     }
 }
